76ac8870e7c6d20869b1cc3a56ebca0f425bd5fd
borgaveakshay <borgave.akshay@gmail.com>
2024-11-25 10:56:15 +0200

    initial commit

--- app/src/androidTest/java/com/example/mintosassignment/datastore/DataStoreTest.kt
+++ app/src/androidTest/java/com/example/mintosassignment/datastore/DataStoreTest.kt
@@ -9,7 +9,6 @@ import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
